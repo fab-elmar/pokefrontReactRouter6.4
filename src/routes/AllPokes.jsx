5622d9fe5064ce91a9ecc1a2b622fb1be83be525
Victorfernandezsaez <v.ferna.saez@gmail.com>
2022-11-10 11:36:23 +0100

    a

--- src/routes/AllPokes.jsx
+++ src/routes/AllPokes.jsx
@@ -8,7 +8,7 @@ import axios from 'axios'
 export async function Allloader() {
     const pokes = await axios.get("https://pokeback-vbf.herokuapp.com/pokemon").then(response => response.data)
     return { pokes }
-}
+    }
 
 
 
@@ -19,7 +19,7 @@ export const AllPokes = () => {
 
     return(
         <> 
-        <h2 class="text-8xl tracking-wider font-bold tracking-tight text-black text-center pt-5 leading-relaxed bg-gradient-to-r from-indigo-500 via-purple-500 to-fuchsia-500">ALL POKES</h2>
+        <div class="text-8xl tracking-wider font-bold tracking-tight text-black text-center pt-5 leading-relaxed bg-gradient-to-r from-indigo-500 via-purple-500 to-fuchsia-500">ALL POKES</div>
         <div className='flex-wrap  flex justify-between items-center p-14 px-24 bg-gradient-to-r from-indigo-500 via-purple-500 to-fuchsia-500 '>
             {pokes.map((p) => <div className='drop-shadow-lg border rounded-lg mb-2.5 p-4 flex flex-col items-center bg-center bg-cover bg-[url("https://wallpaperaccess.com/full/271163.jpg")]'>
             <img className='h-[400px]' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`}/>
@@ -31,7 +31,5 @@ export const AllPokes = () => {
         </div>
         </>
         )
-
-
-}
+    }
 
